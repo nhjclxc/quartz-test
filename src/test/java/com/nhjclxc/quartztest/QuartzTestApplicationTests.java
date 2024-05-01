8cd998ee8d9539a62787595cdd705383660ea7d7
LuoXianchao <nhjclxc@163.com>
2024-05-01 13:42:39 +0800

    初始化项目

---
+++ src/test/java/com/nhjclxc/quartztest/QuartzTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhjclxc.quartztest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuartzTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
